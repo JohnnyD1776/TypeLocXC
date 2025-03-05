d0acfde8a97d021fea39ba8d5e04c1d0a5f67c62
jdurcancoinpass <jdurcan@coinpass.com>
2025-03-05 16:35:49 +0000

    Add XCode Plugin Support

--- Plugins/TypeLocXCPlugin/plugin.swift
+++ Plugins/TypeLocXCPlugin/plugin.swift
@@ -1,9 +1,32 @@
 import PackagePlugin
 import Foundation
-import XcodeProjectPlugin
 
 @main
-struct TypeLocXCPlugin: XcodeBuildToolPlugin {
+struct TypeLocXCPlugin: BuildToolPlugin {
+  func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+    // Locate the TypeLocXC tool
+    guard let target = target.sourceModule else { return [] }
+    let inputFiles = target.sourceFiles.filter({ $0.path.extension == "xcstrings" })
+    return try inputFiles.map {
+      let inputFile = $0
+      let inputPath = inputFile.path
+      let outputName = inputPath.stem + ".swift"
+      let outputPath = context.pluginWorkDirectory.appending(outputName)
+      return .buildCommand(
+        displayName: "Generating \(outputName) from \(inputPath.lastComponent)",
+        executable: try context.tool(named: "TypeLocXC").path,
+        arguments: [ "--source", "\(inputPath)",  "--destination", "\(outputPath)" ],
+        inputFiles: [ inputPath, ],
+        outputFiles: [ outputPath ]
+      )
+    }
+  }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension TypeLocXCPlugin: XcodeBuildToolPlugin {
   func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
     // Filter input files with the ".xcstrings" extension
     let inputFiles = target.inputFiles.filter { $0.path.extension == "xcstrings" }
@@ -23,3 +46,5 @@ struct TypeLocXCPlugin: XcodeBuildToolPlugin {
     }
   }
 }
+#endif
+
