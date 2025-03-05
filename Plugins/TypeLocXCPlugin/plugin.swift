cdfe7ebd7639dff0dc716eab8a81a27d03b6f1dd
jdurcancoinpass <jdurcan@coinpass.com>
2025-03-05 16:35:49 +0000

    Add XCode Plugin Support

--- Plugins/TypeLocXCPlugin/plugin.swift
+++ Plugins/TypeLocXCPlugin/plugin.swift
@@ -1,23 +1,24 @@
 import PackagePlugin
 import Foundation
+import XcodeProjectPlugin
 
 @main
-struct TypeLocXCPlugin: BuildToolPlugin {
-  func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-    // Locate the TypeLocXC tool
-    guard let target = target.sourceModule else { return [] }
-    let inputFiles = target.sourceFiles.filter({ $0.path.extension == "xcstrings" })
-    return try inputFiles.map {
-      let inputFile = $0
+struct TypeLocXCPlugin: XcodeBuildToolPlugin {
+  func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+    // Filter input files with the ".xcstrings" extension
+    let inputFiles = target.inputFiles.filter { $0.path.extension == "xcstrings" }
+
+    // Generate build commands for each input file
+    return try inputFiles.map { inputFile in
       let inputPath = inputFile.path
       let outputName = inputPath.stem + ".swift"
       let outputPath = context.pluginWorkDirectory.appending(outputName)
       return .buildCommand(
         displayName: "Generating \(outputName) from \(inputPath.lastComponent)",
         executable: try context.tool(named: "TypeLocXC").path,
-        arguments: [ "--source", "\(inputPath)",  "--destination", "\(outputPath)" ],
-        inputFiles: [ inputPath, ],
-        outputFiles: [ outputPath ]
+        arguments: ["--source", "\(inputPath)", "--destination", "\(outputPath)"],
+        inputFiles: [inputPath],
+        outputFiles: [outputPath]
       )
     }
   }
